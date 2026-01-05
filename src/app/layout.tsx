a149ad87813bfed3d2ab4871d11364d559a73671
Austin Griffith <austin@ethereum.org>
2026-01-05 16:52:13 -0700

    Update page title to Ethereum Arcology

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata, Viewport } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Arcology | Future Cities",
+  title: "Ethereum Arcology",
   description: "Welcome to Arcology - Building sustainable cities of tomorrow",
 };
 
